66ae0e95c2662b3089f82aca901c7a9f33e5c0a3
branRoberts <brandonkrob12@gmail.com>
2026-04-21 10:01:37 -0400

    Second commit created to read each line from the csv and store into variables and making a new object

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,4 +1,40 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class PayrollCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        try {
+            String regex = "\\|";
+
+            FileReader file = new FileReader("employees.csv");
+            BufferedReader bufferedReader = new BufferedReader(file);
+
+
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] fields = line.split(regex);
+                int id = Integer.parseInt(fields[0]);
+                String name = fields[1];
+                double hours = Double.parseDouble(fields[2].trim());
+                double salary = Double.parseDouble(fields[3].trim());
+
+                Employee employee = new Employee(id,name,hours,salary);
+                for (int i = 0; i < fields.length; i++) {
+                    System.out.println(fields[i]);
+
+                }
+
+            }
+
+        }catch (Exception e){
+            System.out.println("Invalid input");
+        }
+
+    }
 }
