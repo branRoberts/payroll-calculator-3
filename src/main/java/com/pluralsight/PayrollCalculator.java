e3eff4d84607f9f98985bfb0069a74988d434ab6
branRoberts <brandonkrob12@gmail.com>
2026-04-21 10:44:01 -0400

    Foruth Commit i am very lost on this assignemnt and used the instructors code instead as he was teaching

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -7,10 +7,10 @@ import java.util.Scanner;
 public class PayrollCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        String filename = "employees.csv";
         try {
             String regex = "\\|";
-            String filename = "employees.csv";
+
 
             BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
 
@@ -22,18 +22,20 @@ public class PayrollCalculator {
                 int id = Integer.parseInt(fields[0]);
                 String name = fields[1];
                 double hours = Double.parseDouble(fields[2].trim());
-                double payrate = Double.parseDouble(fields[3].trim());
+                double payrate = Double.parseDouble(fields[3]);
 
                 Employee employee = new Employee(id,name,hours,payrate);
+
+                System.out.printf("Employee ID: %d, Name: %s, Gross Pay: $%.2f",id,name,hours,payrate);
                 for (int i = 0; i < fields.length; i++) {
                     System.out.println(fields[i]);
 
                 }
 
             }
-
+            bufferedReader.close();
         }catch (Exception e){
-            System.out.println("Error Reading File");
+            System.out.println("Error Reading File" + filename);
         }
 
     }
